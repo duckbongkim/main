0ed5f743dfbf892adf3f842abaec58ef8757f97f
wooooo-jin <zheld7708@gmail.com>
2024-12-27 18:19:15 +0900

    navbar 만들기, 검색 폼 제작, 캐러셀을 이용한 광고 배너 제작 완료 2024-12-27

---
+++ drunkard/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+  <div class="header_bgimg">
+     
+  </div>
+  <router-view/>
+</div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.header_bgimg{
+  width: 100%;
+  height: 150px;
+}
+</style>
