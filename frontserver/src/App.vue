f1e762825589d724ef68617588dd6876f66322a8
BigFlat98 <hdj9836@naver.com>
2024-12-27 21:53:17 +0900

    프론트 추가. 작업된 것 하나도 없음. 일단 표준으로 사용하면 어떨까 싶은...

---
+++ frontserver/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <HeaderLayout />
+    <div class="container">
+      <router-view/> <!--여기는 라우터를 통해 호출되는 컴포넌트를 띄우겠다는 코드. 이게 라우터를 호출하거나 하는건아님. 라우터 호출을 main.js에서 -->
+    </div>
+    <FooterLayout />
+  </div>
+</template>
+
+<script>
+import HeaderLayout from '@/components/layout/HeaderLayout.vue'
+import FooterLayout from '@/components/layout/FooterLayout.vue'
+  export default{
+    components:{
+        'HeaderLayout':HeaderLayout,
+        'FooterLayout':FooterLayout,
+    }
+  }
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+span{
+  color:#31a344;
+}
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+
+<!--router-link , router-view -> 다 vue에서 사용하는 예약어-->
---
+++ frontserver/src/views/MainView.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+</div>
+</template>
+
+
+<script>
+export default{ 
+    name:'',
+    components:{},
+    computed:{},
+    data(){
+        return{
+
+        };
+    },
+    setup(){},
+    created(){},
+    mounted(){},
+    unmounted(){},
+    methods:{},
+    watch:{}
+}
+</script>
