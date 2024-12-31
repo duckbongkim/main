5682f3b66b6c9fb1b3bd82b372342bf15a7dd14c
duckbongkim <dongjin1508@gmail.com>
2024-12-31 16:35:01 +0900

    Delete backserver directory

--- backserver/routers/router_admin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {getUsers,modifyUser,deleteUser} = require('../controllers/controller_admin.js');
-
-router.get('/users',getUsers);
-router.patch('/users',modifyUser);
-router.delete('/users/:id',deleteUser);
-
-module.exports = router;
