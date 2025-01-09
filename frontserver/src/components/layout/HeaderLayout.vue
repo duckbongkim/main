f94ea056467f56de9ee04424f104289769e06245
choidongjin <dongjin1508@gmail.com>
2025-01-09 12:53:18 +0900

    pull sub 를 위한 커밋

--- frontserver/src/components/layout/HeaderLayout.vue
+++ frontserver/src/components/layout/HeaderLayout.vue
@@ -45,9 +45,7 @@
         <li class="nav-item" v-if="checkAdmin">
           <a class="nav-link active" aria-current="page" @click="goToAdmin('/admin')">Admin</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" @click="goToMenu('/products')">productDetail</a>
-        </li>
+        
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" role="button" aria-expanded="false">
             주류
