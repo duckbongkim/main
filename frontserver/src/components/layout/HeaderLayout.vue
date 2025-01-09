74899c588c1716770462b4db435448135a6dae92
choidongjin <dongjin1508@gmail.com>
2025-01-09 18:01:41 +0900

    18:01 최신상태 커밋

--- frontserver/src/components/layout/HeaderLayout.vue
+++ frontserver/src/components/layout/HeaderLayout.vue
@@ -18,14 +18,6 @@
         <ul class="navbar-nav">
           <li class="nav-item" v-if="checkAdmin">
             <a class="nav-link active" aria-current="page" @click="goToAdmin('/admin')">Admin</a>
-      <a class="navbar-brand" @click="goToMenu('/')">Home</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-          <li class="nav-item" v-if="checkAdmin">
-            <a class="nav-link active" aria-current="page" @click="goToAdmin('/admin')">Admin</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" role="button" aria-expanded="false">
