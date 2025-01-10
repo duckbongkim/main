24a0abc1b48cd2feb20938dddff0e74db90b1363
choidongjin <dongjin1508@gmail.com>
2025-01-10 14:48:53 +0900

    sub에 머지

--- backserver/routers/router_list.js
+++ backserver/routers/router_list.js
@@ -3,37 +3,53 @@ const Products = require('../models/model_products.js')
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql2');
+const { sequelize } = require('../models/model_index.js');
 
 //MySQL 연결 설정
-// const productList = mysql.createConnection({
-//   host: 'localhost',
-//   user: process.env.DB_USER,   
-//   password: process.env.DB_PASSWORD, 
-//   database: process.env.DB_NAME
-// });
+const productList = mysql.createConnection({
+  host: '192.168.250.77',
+  user: process.env.DB_USER,   
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME
+});
 
-// productList.connect((err) => {
-//   if (err) {
-//     console.error('연결 실패:', err);
-//     return;
-//   }
-//   console.log('연결 성공');
-// });
+productList.connect((err) => {
+  if (err) {
+    console.error('연결 실패:', err);
+    return;
+  }
+  console.log('연결 성공');
+});
 
 
 //상품 조회 (리스트 품목 이동 시 데이터 보여주기)
 
-router.get('/', async (req, res) => {
-  const query = 'SELECT * FROM products'; 
+// router.get('/', async (req, res) => {
+//   const query = 'SELECT * FROM products'; 
 
-  try{
-      const [results] = await Products.promise().query(query);
-      console.log('상품 목록:', results); // 데이터 연결 확인
-      res.json(results);
+//   try{
+//       const [results] = await Products.sequelize.query(query);
+//       console.log('상품 목록:', results); // 데이터 연결 확인
+//       res.json(results);
    
-     } catch (err) {
-      console.error('상품 목록 조회 실패:', err);
-      return res.status(500).json({ error: '상품 목록 조회에 실패했습니다.' });
+//      } catch (err) {
+//       console.error('상품 목록 조회 실패:', err);
+//       return res.status(500).json({ error: '상품 목록 조회에 실패했습니다.' });
+//   }
+// });
+
+router.get('/', async (req, res) => {
+  const query = 'SELECT * FROM products'; // MySQL 쿼리
+
+  try {
+    // sequelize.query로 쿼리 실행
+    const [results, metadata] = await sequelize.query(query);
+
+    console.log('상품 목록:', results); // 데이터 연결 확인
+    res.json(results);
+  } catch (err) {
+    console.error('상품 목록 조회 실패:', err);
+    return res.status(500).json({ error: '상품 목록 조회에 실패했습니다.' });
   }
 });
 
